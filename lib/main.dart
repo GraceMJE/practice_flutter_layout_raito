9f56d4815c8667ac3fdd8b262d0cc37fdb2432d4
GraceMJE <akwndms123@naver.com>
2023-10-08 20:40:46 +0900

    Container Expanded Container Flexible Container 의 차이 checking

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,13 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Flexible(flex: 1, child: Container(color: Colors.red, )),
-        Flexible(flex: 2, child: Container(color: Colors.blue, )),
-        Flexible(flex: 3, child: Container(color: Colors.green, )),
-        Flexible(flex: 4, child: Container(color: Colors.yellow, )),
+        Container(width: double.infinity, height: 200, color: Colors.red,),
+        Expanded(child: Container(color: Colors.green, height: 100,),),
+        Flexible(flex: 1, child: Container(color: Colors.blue, height: 100,)),
+        // Flexible(flex: 1, child: Container(color: Colors.red, )),
+        // Flexible(flex: 2, child: Container(color: Colors.blue, )),
+        // Flexible(flex: 3, child: Container(color: Colors.green, )),
+        // Flexible(flex: 4, child: Container(color: Colors.yellow, )),
         // Flexible 차지할 수 있는 공간의 최대 한도 차지
         // flex 키워드로 비율 조정 가능!
         // Flexible은 내부객체 설정값에 영향 받음
